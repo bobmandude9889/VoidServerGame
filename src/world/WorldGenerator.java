223d8060942c1bc9e26797e26c7364e7110c76e1
Will Garrett <willgarrett610@gmail.com>
2015-09-14 17:25:02 -0600

    Changed world generator

--- src/world/WorldGenerator.java
+++ src/world/WorldGenerator.java
@@ -1,29 +1,68 @@
 package world;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import voidservergame.ConfigHandler;
+import voidservergame.Util;
 import voidservergame.VoidServerGame;
 
-class WorldGenerator {
+public class WorldGenerator {
     
     public static boolean isGenerating = false;
     private static Thread generationThread = null;
+    private static Thread bossSpawnThread = null;
+    public static boolean running = true;
     
-    public static void generate(){
-        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "mv create "
-                + VoidServerGame.instance.getConfig().getString("world_name")
-                + " -g end");
+    public static void startThreads(){
+        generationThread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                while(running){
+                    try {
+                        isGenerating = true;
+                        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "mv create "
+                                + ConfigHandler.getWorldName()
+                                + " end");
+                        Thread.sleep(Util.convertTime(Util.MINUTE, Util.MINUTE, 1d).longValue());
+                        populateEmerald(Bukkit.getWorld(ConfigHandler.getWorldName()));
+                        isGenerating = false;
+                        Thread.sleep(Util.convertTime(Util.HOUR, Util.MILISECOND, ConfigHandler.getRegenerationInterval()).longValue());
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+        generationThread.start();
+        
+        bossSpawnThread = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                try {
+                    Thread.sleep(Util.convertTime(Util.HOUR, Util.MILISECOND, ConfigHandler.getBossSpawnInterval()).longValue());
+                    spawnBoss(Bukkit.getWorld(ConfigHandler.getWorldName()));
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        bossSpawnThread.start();
     }
     
-    public static void populateEmerald(final World world){
-        isGenerating = true;
-        generationThread = new Thread(new Runnable(){
-        	@Override
-        	public void run() {
+    @SuppressWarnings("deprecation")
+	private static void deleteThread(){
+            running = false;
+            generationThread.stop();
+            bossSpawnThread.stop();
+    }
+    
+    public static void populateEmerald(World world){
         		VoidServerGame main = VoidServerGame.instance;
-                int emeraldChance = main.getConfig().getInt("emerald_generate_chance");
+                double emeraldChance = ConfigHandler.getEmeraldChance();
                 int replaceArea = 100;
                 int replaceHeight = 70;
                 Location center = new Location(world,0,0,0);
@@ -43,20 +82,10 @@ class WorldGenerator {
                         }
                     }
                 }
-                isGenerating = false;
-                deleteThread();
-        	}
-        });
-        generationThread.start();
-    }
-    
-    @SuppressWarnings("deprecation")
-	private static void deleteThread(){
-    	generationThread.stop();
     }
     
     public static void spawnBoss(World world){
-        
+        // TODO needs a schematic reader
     }
     
 }
